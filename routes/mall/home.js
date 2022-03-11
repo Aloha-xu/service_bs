ea2d8660fedb0c328a2411810b581a86f041df12
Alohaxuuuu <598392491@qq.com>
2022-03-11 10:36:28 +0800

    修复了购物车不显示红点

--- routes/mall/home.js
+++ routes/mall/home.js
@@ -8,11 +8,9 @@ let db = require("../../config/mysql");
  */
 
 router.get("/appInfo", async (req, res) => {
-  // let {uId} = req.query
   let data = {
     banner: {},
     categoryList: [],
-    cartCount: 0,
   };
   //拿到banner数据
   let sql = `SELECT * FROM banner`;
@@ -29,13 +27,6 @@ router.get("/appInfo", async (req, res) => {
       categoryList[i].cateId
     );
   }
-
-  //判断登陆没有 无登录 返回0 有登陆才执行
-  //判断未写
-
-  // sql = `select * from cart where uId=?`
-  // let cartcount = await db.query(sql, String(uId));
-  // data.cartCount = cartcount.length
 
   res.json({
     msg: "success!",
